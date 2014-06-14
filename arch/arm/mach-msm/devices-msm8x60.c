421b91a7dab031d66e8ab8a60bbc2659d1b1fd5e
Warren <deed821@gmail.com>
2014-06-14 20:24:34 +1000

    GPU overclock

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -836,6 +836,16 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
+			.gpu_freq = 320000000,
+			.bus_freq = 6,
+			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 300000000,
+			.bus_freq = 5,
+			.io_fraction = 0,
+		},
+		{
 			.gpu_freq = 266667000,
 			.bus_freq = 4,
 			.io_fraction = 0,
@@ -861,7 +871,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 0,
-	.num_levels = 5,
+	.num_levels = 7,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.strtstp_sleepwake = true,
